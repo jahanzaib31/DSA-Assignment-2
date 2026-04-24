ac2e782d053306803e82046283b78d4ccd76ee85
jahanzaib31 <jahanzaibijaz31@gmail.com>
2026-04-24 20:13:00 +0500

    binary search

--- Muhammad_Jahanzaib_S2025332013.cpp
+++ Muhammad_Jahanzaib_S2025332013.cpp
@@ -1,23 +1,66 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// Code for linear search
+
+// int main() {
+//     int arr[5] = {10, 20, 30, 40, 50};
+//     int key = 30;
+//     bool found = false;
+
+
+//     for(int i = 0; i < 5; i++) {
+//         if (arr[i] == key) {
+//             cout << "Key is found at index" << i ;
+//             found = true;
+//             break;
+//         }
+//     }
+
+//     if (!found) {
+//         cout << "Element not found" << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// Code for binary search
+
+#include <iostream>
 using namespace std;
 
 int main() {
-    int arr[5] = {10, 20, 30, 40, 50};
-    int key = 30;
-    bool found = false;
 
+    int arr[5] = {10,20,30,40,50};
+    int key = 40;
 
-    for(int i = 0; i < 5; i++) {
-        if (arr[i] == key) {
-            cout << "Key is found at index" << i ;
-            found = true;
+    int start = 0, end = 4;
+    int mid, index = -1;
+
+    while(start <= end) {
+        mid = (start + end) / 2;
+
+        if (arr[mid] == key) {
+            index = mid;
             break;
+        }
+
+        else if (key < arr[mid]) {
+            end = mid - 1;
+        }
+
+        else {
+            start = mid + 1;
         }
     }
 
-    if (!found) {
-        cout << "Element not found" << endl;
-    }
+
+    if (index != -1)
+        cout << "Element found at index:" << index;
+
+    else 
+        cout << "Element not found";
 
     return 0;
 }
